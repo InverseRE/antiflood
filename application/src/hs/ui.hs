2e2f34582c0617e45daa8c6533b54dd190e79d4b
Inverse <inverse@ua.fm>
2019-09-06 20:58:43 +0300

    License.

--- application/src/hs/ui.hs
+++ application/src/hs/ui.hs
@@ -1,1 +1,22 @@
+{-
+   Antiflood Copyright (C) 2019 Alexey <Inverse> Shumeiko
+
+   This file is part of Antiflood project.
+
+   This firmware is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This firmware is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with the firmware. If not, see <http://www.gnu.org/licenses/>.
+
+   For more details see LICENSE file.
+-}
+
 module UI where
